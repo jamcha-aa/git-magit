d7495863b07336e378918afa28e6453448be2159
jamcha <jamcha.aa@gmail.com>
2018-09-05 02:12:39 +0900

    Build vuepress

--- .vuepress/summary.js
+++ .vuepress/summary.js
@@ -2,7 +2,7 @@ exports.genSidebarConfig = function () {
     return [
             {
                 title: 'プロローグ',
-                children: ['000a']
+                children: ['000a', '001']
             }
     ]
 }
